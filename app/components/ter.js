fdc3ce2d19a4b83b8cc358e64e31c1a0a84cb4d5
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-06-08 00:48:51 +0100

    added loop.js

--- app/components/ter.js
+++ app/components/ter.js
@@ -2,3 +2,15 @@
 let age = 20;
 let result = age > 5 ? "you are" : "your not";
 console.log(result);
+
+let score = 60;
+let outCome =
+  score > 50
+    ? "go baby"
+    : score > 80
+    ? "you are wrong"
+    : score > 40
+    ? "your go"
+    : "worse";
+
+console.log(outCome);
