26cb307768170b9d970058b2a00cc05275e6caa0
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-06-08 16:58:59 +0100

    added while loop

--- app/components/loop.js
+++ app/components/loop.js
@@ -1,4 +1,26 @@
 // for loop
-for(let i= 1; i < 10; i++) {
+/*for(let i= 1; i < 10; i++) {
      console.log("Greatman")
 }
+for(let i = 1; i < 10; i++) {
+console.log(i)
+} */
+
+/*let coding = ["joy","go", "come", "lena"]
+for(let i = 0; i < coding.length; i++){
+     console.log(coding[i])
+}
+
+for(let i = 1; i <= 5; i++) {
+    console.log(i)
+    for(let j = 1; j <= 3; j++) {
+        console.log("peace" + j)
+    }
+} */
+   
+    //while loop
+    let i = 0
+    while(i<10){
+        console.log(i)
+        i++
+    }
