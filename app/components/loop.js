46f33967761bd1cd9cbea4c70bf84bc668c9c024
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-06-08 17:05:16 +0100

    do...while loop

--- app/components/loop.js
+++ app/components/loop.js
@@ -19,8 +19,16 @@ for(let i = 1; i <= 5; i++) {
 } */
    
     //while loop
-    let i = 0
-    while(i<10){
-        console.log(i)
-        i++
-    }
+    // let i = 0
+    // while(i<10){
+    //     console.log(i)
+    //     i++
+    // }
+
+    //do ...while loop
+
+    let i = 1
+    do{
+console.log(i)
+    i++
+    } while(i<5)
