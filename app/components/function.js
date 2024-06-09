6b1e5219c492d98d186d5859dc3d6a371b6b1c89
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-06-09 21:25:11 +0100

    added function.js

--- app/components/function.js
+++ app/components/function.js
@@ -1,5 +1,28 @@
 //function expression
-const peter = function(){
+/*const peter = function(){
     console.log("i am okay")
 }
-peter()
+peter() */
+
+// function declaration
+
+/*function Name(age,name){
+    console.log(`i am ${age} years old`)
+    console.log(`my name is ${name}`)
+}
+Name(26,"Precious")
+Name(12,"Blessing") */
+
+function isEven(number){
+    if(number % 2 === 0) {
+        return true
+    } else {
+        return false
+    }
+}
+console.log(isEven(4))
+// short cut
+function isOdd(number){
+  return  number % 2 === 1? true :false
+}
+console.log(isOdd(5))
