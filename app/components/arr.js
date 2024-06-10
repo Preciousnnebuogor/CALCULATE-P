ea82ff1d39abfb3675ecc64b4781a8782b94d13d
Preciousnnebuogor <osemekepreciousnnebuogor@gmail.com>
2024-06-10 22:18:39 +0100

    added arr.js

---
+++ app/components/arr.js
@@ -0,0 +1,14 @@
+const myArray = []
+
+myArray[0] = 'apple';
+myArray[1] =  34;
+myArray[2] = true;
+myArray[3] = 22;
+
+// console.log(myArray)
+// console.log(myArray.length)
+// console.log(myArray[1])
+// myArray.push("gold")
+// console.log(myArray)
+myArray.splice(2)
+console.log(myArray)
